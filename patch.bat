62c2a4a813ed8c26b55c0d9c1c91ea7c819d4de9
cielthephantom2 <48162224+cielthephantom2@users.noreply.github.com>
2025-09-01 00:33:28 +0700

    update

--- patch.bat
+++ patch.bat
@@ -6,13 +6,16 @@ color 0A
 
 :: Đọc đường dẫn từ config.ini
 set "ggpk_path="
-for /f "tokens=1,* delims==" %%a in (config.ini) do (
+for /f "usebackq tokens=1,* delims==" %%a in ("config.ini") do (
     if "%%a"=="ggpk_path" set "ggpk_path=%%b"
 )
 
+:: Loại bỏ dấu ngoặc kép nếu có trong file config
+set ggpk_path=%ggpk_path:"=%
+
 :: Kiểm tra xem đường dẫn có tồn tại không
 if not exist "%ggpk_path%" (
-    echo Không tìm thấy file content.ggpk tại: %ggpk_path%
+    echo Không tìm thấy file content.ggpk tại: "%ggpk_path%"
     echo Vui lòng kiểm tra lại file config.ini
     pause
     exit /b 1
@@ -25,14 +28,14 @@ echo ==============================
 echo.
 echo 1. BASIC (Reveal map/ zoom / fog)
 echo 2. REMOVE (Light , Effect , Grass, Shadow, Deadbody)
-echo 3. BLACKSCREEN
+echo 3. PARTICLES
 echo 4. Thoát
 echo.
 
 set /p choice="Chọn (1-4): "
 if "%choice%"=="1" goto basic
 if "%choice%"=="2" goto remove
-if "%choice%"=="3" goto blackscreen
+if "%choice%"=="3" goto particles
 if "%choice%"=="4" goto end
 
 echo Lựa chọn không hợp lệ!
@@ -51,9 +54,9 @@ PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0remove.zip"
 popd
 goto success
 
-:blackscreen
+:particles
 pushd "%~dp0tool\"
-PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0blackscreen.zip"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0particles.zip"
 popd
 goto success
 
