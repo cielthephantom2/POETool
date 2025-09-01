a9d6c8a54e5e2b39a3140a0013177cfd41cd9aeb
cielthephantom2 <48162224+cielthephantom2@users.noreply.github.com>
2025-09-01 08:28:10 +0700

    update

--- patch.bat
+++ patch.bat
@@ -26,17 +26,25 @@ echo ==============================
 echo    MENU CÀI ĐẶT MOD POE2
 echo ==============================
 echo.
-echo 1. BASIC (Reveal map/ zoom / fog)
-echo 2. REMOVE (Light , Effect , Grass, Shadow, Deadbody)
+echo 1. BASIC (Reveal map / fog)
+echo 2. REMOVE (Light , Effect , Grass, Shadow)
 echo 3. PARTICLES
-echo 4. Thoát
+echo 4. Remove Dead body
+echo 5. Zoom x0.3
+echo 6. Zoom x0.6
+echo 7. Zoom x0.9
+echo 8. Thoát
 echo.
 
-set /p choice="Chọn (1-4): "
+set /p choice="Chọn (1-8): "
 if "%choice%"=="1" goto basic
 if "%choice%"=="2" goto remove
 if "%choice%"=="3" goto particles
-if "%choice%"=="4" goto end
+if "%choice%"=="4" goto deadbody
+if "%choice%"=="5" goto zoom3
+if "%choice%"=="6" goto zoom6
+if "%choice%"=="7" goto zoom9
+if "%choice%"=="8" goto end
 
 echo Lựa chọn không hợp lệ!
 pause
@@ -44,19 +52,43 @@ goto menu
 
 :basic
 pushd "%~dp0tool\"
-PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0basic.zip"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\basic.zip"
 popd
 goto success
 
 :remove
 pushd "%~dp0tool\"
-PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0remove.zip"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\remove.zip"
 popd
 goto success
 
 :particles
 pushd "%~dp0tool\"
-PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0particles.zip"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\particles.zip"
+popd
+goto success
+
+:deadbody
+pushd "%~dp0tool\"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\deadbody.zip"
+popd
+goto success
+
+:zoom3
+pushd "%~dp0tool\"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\zoom3.zip"
+popd
+goto success
+
+:zoom6
+pushd "%~dp0tool\"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\zoom6.zip"
+popd
+goto success
+
+:zoom9
+pushd "%~dp0tool\"
+PatchBundledGGPK3.exe "%ggpk_path%" "%~dp0patch\zoom9.zip"
 popd
 goto success
 
